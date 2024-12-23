8c3c90e8ada8b4ad4f2aaf2e88c8ada5dc312c1a
Your Name <ahmaddabeer36@gmail.com>
2024-12-24 00:19:06 +0530

    add new concept of python

---
+++ CHAPTER PROBLEM SET/chapter 2/operator.py
@@ -0,0 +1,46 @@
+# Arthmetic Operators
+a = 10
+b = 20
+print(a + b) #30
+
+print(a - b) #-10
+
+
+#Assingment Operators
+
+a =  4-2 #Assin 4-2 in a
+
+
+#Comparison Operators
+
+a = 10
+b = 20
+c= a<b#comparison opetaor
+print(c) #True
+print(" ")
+ 
+ 
+print("Logical and  Operators")
+
+a= True and False
+b = True and True
+c= False and False
+d = True and False
+print(a) #False
+print(b) #True
+print(c) #False
+print(d) #True
+
+print(" ")
+
+print("Logical or  Operators")
+
+a= True or False
+b = True or True
+c= False or False
+d = True or False
+print(a) #True
+print(b) #True
+print(c) #False
+print(d) #True
+
---
+++ CHAPTER PROBLEM SET/chapter 2/typefunction_and_castiong.py
@@ -0,0 +1,26 @@
+# Type()function and Typecastiong
+print("TYPE() FUNCTION")
+print(" ")
+a= 31
+t = type(a)
+print(t)
+
+print(" ")
+print("TYPECASTING")
+print(" ")
+
+a= " 123"
+t = type(a)
+
+print(t)
+
+b = int(a)
+
+
+d= type(b)
+
+print(d)
+
+print(b)
+
+    
